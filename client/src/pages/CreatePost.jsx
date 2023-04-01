99d4fb8277d9fe9aa44a50484ed41283fb19eb21
rva-mike <mike.levy.rva@gmail.com>
2023-04-01 15:07:25 -0400

    CreatePost styles, generate and share buttons

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,6 +20,10 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const generateImage = () => {
+
+  }
+
   const handleSubmit = () => {
 
   }
@@ -80,7 +84,22 @@ const CreatePost = () => {
         </div>
 
         <div className='mt-5 flex gap-5'>
-          <button></button>
+          <button
+            type='button'
+            onClick={generateImage}
+            className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+
+        <div className='mt-10 '>
+          <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image you want, you can share it with others in the community</p>
+          <button
+            type='submit'
+            className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>
+            {loading ? 'Sharing...' : 'Share with the community'}</button>
+
         </div>
       </form>
     </section>
