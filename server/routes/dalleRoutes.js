e22cc64d973782f04fe5dd528094392a96e02d1f
rva-mike <mike.levy.rva@gmail.com>
2023-04-02 16:02:32 -0400

    generate image success, connected backend to frontend, generateImage async function

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,4 +16,27 @@ router.route('/').get((req, res) => {
     res.send ('Hello from DALL-E');
 })
 
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body
+
+        //getting image
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json'
+        })
+
+        const image = aiResponse.data.data[0].b64_json; 
+
+        //sending image to the frontend
+        res.status(200).json({ photo: image });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message)
+    }
+})
+
 export default router;
