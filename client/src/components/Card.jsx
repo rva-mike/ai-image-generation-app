99fe7a8d97c7a673e397753e4f721a69337c79db
rva-mike <mike.levy.rva@gmail.com>
2023-04-02 17:48:13 -0400

    card styling start

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Card = () => {
+import { download } from '../assets'
+import { downloadImage } from '../utils'
+
+const Card = ({ _id, name, prompt, photo }) => {
   return (
-    <div>Card</div>
+    <div className='rounded-xl group relative shadow-card hover:shadow-cardhover card'>
+      <img className='w-full h-auto object-cover rounded-xl' src={photo} alt={prompt} />
+    </div>
   )
 }
 
